33a61f24d71dd4e1e75313196f032e998bed469a
DucLV08 <duc080999@gmail.com>
2023-02-07 19:10:49 +0700

    skeleton home

--- src/front-end/music-app/src/containers/Loading/Home.tsx
+++ src/front-end/music-app/src/containers/Loading/Home.tsx
@@ -1,9 +1,74 @@
 import React from "react";
-import { Box } from "theme-ui";
+import { Grid, AspectRatio, Box } from "theme-ui";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const LoadingHome = () => {
+	const arr = new Array(3).fill(0);
 	return(
-		<Box></Box>
+		<SkeletonTheme baseColor="rgba(244,246,248,0.05)" highlightColor="rgba(244,246,248,0.05)">
+			<SkeletonSlider />
+			<Box>
+				{arr.map(() => (
+					<SkeletonItem key={Math.random()}/>
+				))}
+			</Box>
+		</SkeletonTheme>
 	);
 }
 export default LoadingHome;
+
+
+const SkeletonSlider = () => {
+	const arrSliider = new Array(3).fill(0);
+	return(
+		<Grid
+			columns={3}
+			gap={20}
+			style={{
+				cursor: 'wait',
+			}}>
+				{arrSliider.map(() => (
+					<Box key={Math.random()}>
+						<AspectRatio ratio={16 / 9}>
+							<Skeleton width={'100%'} height={'100%'} borderRadius={10} />
+						</AspectRatio>
+					</Box>
+				))}
+		</Grid>
+	)
+}
+
+
+const SkeletonItem = () => {
+	const arrSkeletonItem = new Array(5).fill(0);
+	return(
+		<Box sx={{ marginY: '40px'}}>
+			<Skeleton
+				width={'40%'}
+				height={'100%'}
+				baseColor="rgba(244,246,248,0.05)"
+				highlightColor="rgba(244,246,248,0.05)"
+				style={{ marginBottom: '15px'}}
+			/>
+			<Grid
+				columns={5}
+				gap={20}
+				style={{
+					cursor: 'wait',
+				}}
+			>
+				{arrSkeletonItem.map(() => (
+					<Box key={Math.random()}>
+						<AspectRatio ratio={1/1}>
+							<Skeleton width={'100%'} height={'100%'} borderRadius={10}/>
+						</AspectRatio>
+						<Skeleton width={'80%'} height={'8%'} style={{ margin: '8px 0'}}/>
+						<Skeleton width={'100%'} height={'8%'} />
+						<Skeleton width={'100%'} height={'8%'} />
+					</Box>
+				))}
+			</Grid>
+		</Box>
+	)
+}
--- src/front-end/music-app/src/pages/index.tsx
+++ src/front-end/music-app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from "react";
 import { Box } from "theme-ui";
 import ReponsiveContainer from "../components/ReponsiveContainer";
 import Slide from "../components/Slide";
+import LoadingHome from "../containers/Loading/Home";
 import getBanners from "../service/getBanners";
 import getPlaylists from "../service/getPlaylists";
 interface DataSlide {
@@ -22,64 +23,73 @@ interface DataSlideShow {
 const Home = () => {
 	const [dataSlide, setDataSlide] = useState<Array<DataSlide>>();
 	const [dataSlideShow, setDataSlideShow] = useState<Array<DataSlideShow>>();
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
-		getBanners.getAll().then(res => {
-			setDataSlide(res.data.items);
-		})
+		setTimeout(() => {
+			getBanners.getAll().then(res => {
+				setDataSlide(res.data.items);
+				setLoading(false);
+			})
+		}, 200)
 		getPlaylists.getAll().then(res => {
 			setDataSlideShow(res.data);
 		})
 	}, [])
 	return(
 		<ReponsiveContainer>
-			<Slide
-				setting={{
-					infinite: true,
-					slidesToShow: 3,
-					slidesToScroll: 1,
-					autoplay: true,
-					autoplaySpeed: 4000,
-					pauseOnHover: true,
-					speed: 200,
-				}}
-				dataSlide={dataSlide?.map((item:DataSlide) => {
-					return {
-						id: item?.encodeId,
-						image: item?.banner,
-						title: '',
-						sortDescription: '',
-					}
-				})}
-				title=""
-				sectionType="banner"
-			/>
-			{dataSlideShow?.map((item:any, index) => {
-				return(
-					<Box key={index}>
-						<Slide
-							setting={{
-								infinite: true,
-								slidesToShow: 5,
-								slidesToScroll: 1,
-								autoplay: false,
-								autoplaySpeed: 0,
-								pauseOnHover: false,
-								speed: 0,
-							}}
-							dataSlide={item?.items?.map((item:DataSlideShow) => {
-								return {
-									id: item?.encodeId,
-									image: item?.thumbnailM,
-									title: item?.title,
-									sortDescription: item?.sortDescription,
-								}
-							})}
-							title={item?.title}
-							sectionType="playlist"
-						/>
-					</Box>
-				);
-			})}
+			{ loading ?
+				<LoadingHome /> :
+				<>
+					<Slide
+						setting={{
+							infinite: true,
+							slidesToShow: 3,
+							slidesToScroll: 1,
+							autoplay: true,
+							autoplaySpeed: 4000,
+							pauseOnHover: true,
+							speed: 200,
+						}}
+						dataSlide={dataSlide?.map((item:DataSlide) => {
+							return {
+								id: item?.encodeId,
+								image: item?.banner,
+								title: '',
+								sortDescription: '',
+							}
+						})}
+						title=""
+						sectionType="banner"
+					/>
+					{dataSlideShow?.map((item:any, index) => {
+						return(
+							<Box key={index}>
+								<Slide
+									setting={{
+										infinite: true,
+										slidesToShow: 5,
+										slidesToScroll: 1,
+										autoplay: false,
+										autoplaySpeed: 0,
+										pauseOnHover: false,
+										speed: 0,
+									}}
+									dataSlide={item?.items?.map((item:DataSlideShow) => {
+										return {
+											id: item?.encodeId,
+											image: item?.thumbnailM,
+											title: item?.title,
+											sortDescription: item?.sortDescription,
+										}
+									})}
+									title={item?.title}
+									sectionType="playlist"
+								/>
+							</Box>
+						);
+					})}
+				</>
+			}
 		</ReponsiveContainer>
 	);
 }
