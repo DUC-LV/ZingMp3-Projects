1eb6fe53415a1eed9e57b90c9a09349e2887e8ed
DucLV08 <duc080999@gmail.com>
2023-02-13 00:06:06 +0700

    api list video and add data

--- src/videos/urls.py
+++ src/videos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('videos/viet-nam/', views.GetListVideoVietNam.as_view()),
+    path('videos/us-uk/', views.GetListVideoUsUk.as_view()),
 ]
--- src/videos/views.py
+++ src/videos/views.py
@@ -5,9 +5,67 @@ from .models import TopicVideos, Videos, VideoOfTopic, ArtistOfVideo
 
 
 # Create your views here.
+# GetListVideoVietNam
 class GetListVideoVietNam(APIView):
     def get(self, request):
         topic = TopicVideos.objects.filter(topic_name='Việt Nam')
+        topic_id = [tp.id for tp in topic]
+        topic_video = VideoOfTopic.objects.filter(topic_id__in=topic_id)
+        items = []
+        for tp_vd in topic_video:
+            video = tp_vd.video
+            video_id = ArtistOfVideo.objects.filter(video_id=video)
+            art_first = video_id[0].artist
+            artist_data = []
+            for artist in video_id:
+                art = artist.artist
+                artist_data.append({
+                    "id": art.get_hash_id(),
+                    "name": art.name,
+                    "spotlight": art.spotlight,
+                    "alias": art.alias,
+                    "thumbnail": art.thumbnail,
+                    "thumbnailM": art.thumbnail_m,
+                    "isOA": art.is_oa,
+                    "isOABrand": art.is_oa_brand,
+                })
+            data_video = {
+                "encodeId": video.get_hash_id(),
+                "title": video.title,
+                "alias": video.alias,
+                "isoffical": video.is_offical,
+                "artistsNames": video.artist_names,
+                "artists": artist_data,
+                "isWorldWide": video.is_world_wide,
+                "thumbnailM": video.thumbnail_m,
+                "thumbnail": video.thumbnail,
+                "duration": video.duration,
+                "streamingStatus": video.streaming_status,
+                "artist": {
+                    "id": art_first.get_hash_id(),
+                    "name": art_first.name,
+                    "spotlight": art_first.spotlight,
+                    "alias": art_first.alias,
+                    "thumbnail": art_first.thumbnail,
+                    "thumbnailM": art_first.thumbnail_m,
+                },
+            }
+            items.append(data_video)
+
+        res = {
+            "err": 0,
+            "msg": "Success",
+            "data": {
+                "items": items
+            }
+        }
+        return JsonResponse(res, safe=False)
+
+
+# GetListVideoUsUk
+class GetListVideoUsUk(APIView):
+    def get(self, request):
+        topic = TopicVideos.objects.filter(topic_name='US-UK')
         topic_id = [tp.id for tp in topic]
         topic_video = VideoOfTopic.objects.filter(topic_id__in=topic_id)
         items = []
